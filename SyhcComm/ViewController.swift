f0354f321de847bcec6db833fafde9b1aeda27d2
Koo2000 <koo2000@gmail.com>
2016-12-11 19:16:25 +0900

    Initial Commit

---
+++ SyhcComm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SyhcComm
+//
+//  Created by 麻野耕一 on 2016/12/11.
+//  Copyright © 2016年 麻野耕一. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
